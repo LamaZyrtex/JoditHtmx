c37729f9d0f1724fb321dfda51a539e4a4644d25
Espen Michael Larød <espen.michael.larod@stortinget.no>
2025-04-17 19:20:03 +0200

    Lagt til norsk som standardspråk

--- client/src/jodit/defaultEditor.ts
+++ client/src/jodit/defaultEditor.ts
@@ -1,6 +1,15 @@
 ﻿import {Jodit} from 'jodit';
+import no from 'jodit/esm/langs/no.js';  
+
 
 export function createJoditEditor(selector: string, value: string): void {
-    const jodit = Jodit.make(selector)
-    jodit.value = value
+    // Importer språkmodul
+    Jodit.lang.no = no;
+    // Lag editor
+    const jodit = Jodit.make(selector,{
+        language : 'no'
+     })
+     // Sett verdi/innhold
+    jodit.value = value;
+
 }
